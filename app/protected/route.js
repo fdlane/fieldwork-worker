f5e1794be98d376d836ab5c762591f72414cab0d
AmberRivera <amberolive@gmail.com>
2017-02-14 14:17:16 -0500

    Remove ghost code.

--- app/protected/route.js
+++ app/protected/route.js
@@ -2,12 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  /*beforeModel() {
-    console.log("Debugging " + this.get('session'));
-
-    if(!this.get('session.isAuthenticated')){
-      console.log("Transitioning to application.");
-      this.transitionTo('application');
-    }
-  }*/
 });
