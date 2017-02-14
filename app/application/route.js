f771eca253fa5d2ec8ab27c4cae9f967833c1413
AmberRivera <amberolive@gmail.com>
2017-02-14 14:11:46 -0500

    Fixed logging out on refresh.

--- app/application/route.js
+++ app/application/route.js
@@ -2,12 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  beforeModel() {
+
+    this.get('session').fetch().catch((error) => {
+      if(error) {
+        console.log("Error fetching session: " + error);
+        this.transitionTo('login');
+      }
+    });
+    
+  },
+
   actions: {
 
     logout() {
 
       this.get('session').close().then(function() {
-        this.transitionTo('application');
+        this.transitionTo('login');
       }.bind(this));
     }
   }
--- app/protected/route.js
+++ app/protected/route.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  beforeModel() {
+  /*beforeModel() {
     console.log("Debugging " + this.get('session'));
+
     if(!this.get('session.isAuthenticated')){
+      console.log("Transitioning to application.");
       this.transitionTo('application');
     }
-  }
+  }*/
 });
