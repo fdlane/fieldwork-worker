9fcd32f26ffcc6161ff6f26c9b3f959ca3e83d79
Amber <amberolive@gmail.com>
2017-03-22 09:49:44 -0400

    Basic ui (#6)
    * Basic ui
    
    * Display jobs based on user and isActive field
    
    * Basic ui
    
    * Display jobs based on user and isActive field

---
+++ tests/integration/components/jobs-table/component-test.js
@@ -0,0 +1,10 @@
+import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('jobs-table', 'Integration | Component | jobs table', {
+  integration: true
+});
+
+test('change when functionality added', function(assert) {
+  assert.ok(true);
+});
---
+++ tests/integration/components/search-box/component-test.js
@@ -0,0 +1,12 @@
+import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('search-box', 'Integration | Component | search box', {
+  integration: true
+});
+
+test('change when functionality added', function(assert) {
+
+  assert.ok(true);
+
+});
