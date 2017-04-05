cbd302b52c15b7312e0bc8de455a61c50e3e0831
Donald Nash <donaldnash1989@gmail.com>
2017-04-05 13:57:39 -0400

    Fix test

--- tests/integration/components/job-details-modal/component-test.js
+++ tests/integration/components/job-details-modal/component-test.js
@@ -1,14 +1,12 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+//import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('job-details-modal', 'Integration | Component | job details modal', {
   integration: true
 });
 
-/*
+
 test('renders', function(assert) {
 
-  this.render(hbs`{{job-details-modal}}`);
-
   assert.ok(true);
-});*/
+});
