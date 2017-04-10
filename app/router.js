d2795ecc281b4bf530a2807646278b44c36aa7db
Zachary Berardo <zachary@zachnthebox.com>
2017-04-10 12:04:54 -0400

    refactor: add loading route for login (#15)

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login');
+  this.route('login', function() {});
   this.route('protected');
   this.route('password-reset');
 });
