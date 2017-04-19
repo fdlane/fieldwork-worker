f22375fbde9c151c2ef5db509a46787523b7c029
RoundDev <mkaminski69@gmail.com>
2017-04-19 14:31:15 -0400

    add git logo and link to repo/ removed buttons from worker table (#16)
    * add git logo and link to repo/ removed buttons from worker table
    
    * Fix tests

--- tests/unit/login/controller-test.js
+++ tests/unit/login/controller-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:login', 'Unit | Controller | login', {
+moduleFor('controller:login.index', 'Unit | Controller | login', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
