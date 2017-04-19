d842a76cc00d7a17864744be8fc67fc10665869d
AmberRivera <amberolive@gmail.com>
2017-04-19 16:10:44 -0400

    Create job service, filter, ember models table

--- app/components/jobs-table/component.js
+++ app/components/jobs-table/component.js
@@ -2,5 +2,49 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    
+  jobService: Ember.inject.service('job'),
+
+  columns: [
+    {
+      "propertyName": "metaId",
+      "title": "Id",
+    },
+
+    {
+      "propertyName": "location",
+      "title": "Location",
+    },
+
+    {
+      "propertyName": "status",
+      "title": "Status",
+    },
+
+    {
+      "propertyName": "category",
+      "title": "Category",
+    },
+
+    {
+      "propertyName": "scheduleDate",
+      "title": "Scheduled",
+    },
+
+    {
+      "propertyName": "createdDate",
+      "title": "Created",
+    }
+  ],
+
+  tableClasses: Ember.Object.create({
+    "table": "table  table-bordered",
+
+  }),
+
+
+  actions: {
+    selectJob(table) {
+      this.get('jobService').selectJob(table);
+    },
+  }
 });
