8ee4ad451a1aae52840964b6c628f973dfa60900
Amber <amberolive@gmail.com>
2017-04-25 19:24:07 -0400

    Add firebaseApp service to job service test

--- tests/unit/job/service-test.js
+++ tests/unit/job/service-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:job', 'Unit | Service | job', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['service:firebaseApp']
 });
 
 // Replace this with your real tests.
